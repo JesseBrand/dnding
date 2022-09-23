7a7ab63cbda46c9e059a577dda284b88257b54ca
Jurrien Fakkeldij <jurrien.fakkeldij@gmail.com>
2022-09-23 22:46:07 +0200

    style: 💄 new styles to use to make it look like the phb

--- src/dnding-sessions/session-view.js
+++ src/dnding-sessions/session-view.js
@@ -1,25 +1,71 @@
-import {LitElement, html, css} from 'lit';
-import {SessionEntry} from './session-entry.js';
-import {getAllSessions} from './session-manager.js'
+import { LitElement, html, css } from "lit";
+import { phbStyles } from "./phb-styles.js";
+import { SessionEntry } from "./session-entry.js";
+import { getAllSessions } from "./session-manager.js";
 
 export class SessionView extends LitElement {
-
   static get properties() {
-    return {};
+    return {
+      selectedSessionId: { type: Number },
+    };
   }
-  
+
+  static get styles() {
+    return [phbStyles];
+  }
+
   constructor() {
     super();
     this.sessions = getAllSessions();
+    this.selectedSessionId = this.sessions.reverse()[0].id;
+    console.log(this.selectedSessionId);
+    console.log(this.sessions);
+  }
+
+  previousSession() {
+    console.log("tes");
+    this.selectedSessionId = this.selectedSessionId - 1;
+  }
+  nextSession() {
+    this.selectedSessionId = this.selectedSessionId + 1;
   }
 
   render() {
+    console.log("render", this.sessions);
+    console.log(
+      "prev",
+      this.selectedSessionId === this.sessions[this.sessions.length - 1].id
+    );
+    console.log("next", this.selectedSessionId === this.sessions[0].id);
     return html`
       <h1>Sessions</h1>
-      ${this.sessions.map(oSession => html`<session-entry id="${oSession.id}" sessionName="${oSession.sessionName}" ocDate="${oSession.ocDate}" icDate="${oSession.icDate}" .characters=${oSession.characters} .happenings=${oSession.happenings} .loot=${oSession.loot} .repercussions=${oSession.repercussions}></session-entry>`)}
+      <button
+        @click="${this.previousSession}"
+        ?hidden="${this.selectedSessionId ===
+        this.sessions[this.sessions.length - 1].id}"
+      >
+        Prev
+      </button>
+      <button
+        @click="${this.nextSession}"
+        ?hidden="${this.selectedSessionId === this.sessions[0].id}"
+      >
+        Next
+      </button>
+      ${this.sessions.map(
+        (oSession) =>
+          html`<div class="phb">
+            <session-entry
+              id="${oSession.id}"
+              ocDate="${oSession.ocDate}"
+              icDate="${oSession.icDate}"
+              .characters=${oSession.characters}
+              .happenings=${oSession.happenings}
+            ></session-entry>
+          </div>`
+      )}
     `;
   }
-
 }
 
-window.customElements.define('session-view', SessionView);
+window.customElements.define("session-view", SessionView);
