c2f8fdfac98e2210d291909627f7891c7637198b
Jurrien Fakkeldij <jurrien.fakkeldij@gmail.com>
2022-09-23 22:46:07 +0200

    style: 💄 new styles to use to make it look like the phb

--- src/dnding-sessions/session-entry.js
+++ src/dnding-sessions/session-entry.js
@@ -1,45 +1,56 @@
-import {LitElement, html, css} from 'lit';
-import {commaAndList} from '../util.js';
+import { LitElement, html, css } from "lit";
+import { commaAndList } from "../util.js";
+import { phbStyles } from "./phb-styles.js";
 
 export class SessionEntry extends LitElement {
   static get styles() {
-    return css`
-    `;
+    return [
+      phbStyles,
+      css`
+        .purple-header {
+          color: purple;
+          text-align: center;
+        }
+      `,
+    ];
   }
 
   static get properties() {
     return {
-      id: {type: Number},
-      ocDate: {type: String},
-      icDate: {type: String},
-      characters: {type: Array},
-      happenings: {type: Array},
+      id: { type: Number },
+      ocDate: { type: String },
+      icDate: { type: String },
+      characters: { type: Array },
+      happenings: { type: Array },
     };
   }
 
   constructor() {
     super();
-    console.log(this, this.characters);
   }
 
   render() {
     return html`
-      <div class="session">
-        <h2>Session ${this.id}: ${this.ocDate}</h2>
-        <p>IC Date: ${this.icDate}</p>
-        <p>The party, consisting of ${commaAndList(this.characters)}:
-          <ul>
-            ${this.happenings.map(sHappening => html`<li>${sHappening};</li>`)}
-          </ul>
-        </p>
+        <span class="topnote">Date: ${this.icDate}</span>
+        <h1>The Title of the Story</h1>
+        <p>The party members</p>
+        <dl>
+          ${this.happenings.map(
+            (sHappening) =>
+              html`<dd>${sHappening};</dd>
+                <br />`
+          )}
+        </dl>
+        <div class="columnSplit"></div>
+        <h5 class="purple-header">The Party</h5>
+
+        <p>${commaAndList(this.characters)}:</p>
+        <span class="pageNumber"></span>
+        <span class="footnote">Session ${this.id}: ${this.ocDate}</span>
       </div>
       <slot></slot>
     `;
   }
-
-  _onClick() {
-    this.count++;
-  }
 }
 
-window.customElements.define('session-entry', SessionEntry);
+window.customElements.define("session-entry", SessionEntry);
