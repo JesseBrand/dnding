bb4a6231b0e109219f644ab70413fe5f19c85624
Jurrien Fakkeldij <jurrien.fakkeldij@gmail.com>
2022-10-01 17:30:02 +0200

    refactor: 💡 moved topnote to display correctly

--- src/dnding-sessions/session-entry.js
+++ src/dnding-sessions/session-entry.js
@@ -31,7 +31,6 @@ export class SessionEntry extends LitElement {
 
   render() {
     return html`
-        <span class="topnote">Date: ${this.icDate}</span>
         <h1>The Title of the Story</h1>
         <p>The party members</p>
         <ul>
