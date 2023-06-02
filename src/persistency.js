8fb9a081a98268ad2b1919b2609a248871b95852
Jesse Brand <jessebrand@gmail.com>
2023-06-02 14:53:15 +0200

    Updated xp after 24/5/23.

--- src/persistency.js
+++ src/persistency.js
@@ -2,24 +2,24 @@ import {LitElement, html, css} from 'lit';
 
 function fetchAllCharacters() {
   return [
-    {name: 'Sam', xp: 35},
-    {name: 'Castle-8', xp: 31},
-    {name: 'Horatio ', xp: 30},
+    {name: 'Sam', xp: 36},
+    {name: 'Castle-8', xp: 32},
+    {name: 'Horatio ', xp: 31},
     {name: 'Alaric', xp: 26},
+    {name: 'Ru', xp: 26},
     {name: 'Nuk', xp: 25},
-    {name: 'Ru', xp: 25},
-    {name: 'Bill', xp: 22},
+    {name: 'Bill', xp: 23},
+    {name: 'Paxton', xp: 23},
+    {name: 'Enphine', xp: 22},
+    {name: 'Sir Dixon', xp: 22},
+    {name: 'Aboouck', xp: 21},
     {name: 'Exilian', xp: 21},
-    {name: 'Paxton', xp: 22},
-    {name: 'Enphine', xp: 21},
-    {name: 'Sir Dixon', xp: 21},
-    {name: 'Aboouck', xp: 20},
+    {name: 'Bavon', xp: 18},
     {name: 'Drathuul', xp: 18},
-    {name: 'Bavon', xp: 17},
-    {name: 'Freya', xp: 17},
-    {name: 'Girard', xp: 17},
-    {name: 'John', xp: 17},
-    {name: 'Navani', xp: 17}
+    {name: 'Freya', xp: 18},
+    {name: 'Girard', xp: 18},
+    {name: 'John', xp: 18},
+    {name: 'Navani', xp: 18}
   ]
 }
 
