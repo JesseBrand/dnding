fbb41d347926582274bbe15ba7308d43c04c1188
Jesse Brand <jessebrand@gmail.com>
2024-01-19 13:43:47 +0100

    Updated after 17/1/24

--- src/persistency.js
+++ src/persistency.js
@@ -1,19 +1,19 @@
 import {LitElement, html, css} from 'lit';
 
-// updated after 22/11/23
+// updated after 17/1/24
 
 function fetchAllCharacters() {
   return [
     {name: 'Sam', xp: 40},
-    {name: 'Castle-8', xp: 38},
-    {name: 'Horatio ', xp: 35},
-    {name: 'Lynel', xp: 35},
+    {name: 'Castle-8', xp: 39},
+    {name: 'Horatio ', xp: 36},
+    {name: 'Lynel', xp: 36},
     {name: 'Ru', xp: 33},
+    {name: 'Bill', xp: 32},
     {name: 'Alaric', xp: 31},
-    {name: 'Bill', xp: 31},
-    {name: 'Enphine', xp: 30},
-    {name: 'Paxton', xp: 30},
-    {name: 'Sir Dixon', xp: 29},
+    {name: 'Enphine', xp: 31},
+    {name: 'Paxton', xp: 31},
+    {name: 'Sir Dixon', xp: 30},
     {name: 'Aboouck', xp: 26},
     {name: 'Nuk', xp: 26},
     {name: 'Exilian', xp: 24},
