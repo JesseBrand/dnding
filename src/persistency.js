3932c0c34ced8af62bda7d4f12680abca5cd4ff2
Jesse Brand <jessebrand@gmail.com>
2025-02-24 08:56:40 +0100

    Backport 27/3/24 changes to old site

--- src/persistency.js
+++ src/persistency.js
@@ -1,29 +1,29 @@
 import {LitElement, html, css} from 'lit';
 
-// updated after 17/1/24
+// updated after 27/3/24
 
-function fetchAllCharacters() {
+function fetchAllCharacters () {
   return [
-    {name: 'Sam', xp: 40},
-    {name: 'Castle-8', xp: 39},
-    {name: 'Horatio ', xp: 36},
-    {name: 'Lynel', xp: 36},
-    {name: 'Ru', xp: 33},
-    {name: 'Bill', xp: 32},
-    {name: 'Alaric', xp: 31},
-    {name: 'Enphine', xp: 31},
-    {name: 'Paxton', xp: 31},
-    {name: 'Sir Dixon', xp: 30},
-    {name: 'Aboouck', xp: 26},
-    {name: 'Nuk', xp: 26},
-    {name: 'Exilian', xp: 24},
-    {name: 'Layden', xp: 23},
-    {name: 'Bavon', xp: 20},
-    {name: 'Drathuul', xp: 20},
-    {name: 'Freya', xp: 20},
-    {name: 'Girard', xp: 20},
-    {name: 'John', xp: 20},
-    {name: 'Navani', xp: 20}
+    { name: 'Sam', xp: 40 },
+    { name: 'Castle-8', xp: 40 },
+    { name: 'Horatio ', xp: 37 },
+    { name: 'Lynel', xp: 37 },
+    { name: 'Ru', xp: 33 },
+    { name: 'Bill', xp: 33 },
+    { name: 'Enphine', xp: 32 },
+    { name: 'Paxton', xp: 32 },
+    { name: 'Alaric', xp: 31 },
+    { name: 'Sir Dixon', xp: 31 },
+    { name: 'Aboouck', xp: 26 },
+    { name: 'Nuk', xp: 27 },
+    { name: 'Exilian', xp: 24 },
+    { name: 'Layden', xp: 23 },
+    { name: 'Bavon', xp: 20 },
+    { name: 'Drathuul', xp: 20 },
+    { name: 'Freya', xp: 20 },
+    { name: 'Girard', xp: 20 },
+    { name: 'John', xp: 20 },
+    { name: 'Navani', xp: 20 }
   ]
 }
 
