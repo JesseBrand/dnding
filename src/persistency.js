ed40877aee821312946dfbaed3968851e3b8448f
Jesse Brand <jessebrand@gmail.com>
2025-06-05 11:11:05 +0200

    Added xp after 4/6/26

--- src/persistency.js
+++ src/persistency.js
@@ -1,6 +1,6 @@
 import {LitElement, html, css} from 'lit';
 
-// updated after 23/4/24
+// updated after 4/6/25
 
 function fetchAllCharacters () {
   return [
@@ -8,10 +8,10 @@ function fetchAllCharacters () {
     { name: 'Sam', xp: 40, dreamXp: 4 },
     { name: 'Horatio ', xp: 40, dreamXp: 1 },
     { name: 'Lynel', xp: 39 },
-    { name: 'Bill', xp: 37, dreamXp: 5 },
-    { name: 'Enphine', xp: 36, dreamXp: 7 },
+    { name: 'Bill', xp: 37, dreamXp: 6 },
+    { name: 'Enphine', xp: 36, dreamXp: 8 },
     { name: 'Paxton', xp: 35 },
-    { name: 'Sir Dixon', xp: 35, dreamXp: 6 },
+    { name: 'Sir Dixon', xp: 35, dreamXp: 8 },
     { name: 'Ru', xp: 34, dreamXp: 5 },
     { name: 'Alaric', xp: 31 },
     { name: 'Nuk', xp: 27 },
@@ -24,7 +24,7 @@ function fetchAllCharacters () {
     { name: 'Girard', xp: 22 },
     { name: 'John', xp: 22 },
     { name: 'Navani', xp: 22 },
-    { name: 'Sir Dozak the Magnificient', xp: 0, dreamXp: 4 }
+    { name: 'Sir Dozak the Magnificient', xp: 0, dreamXp: 5 }
   ]
 }
 
