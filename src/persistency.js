ddc9c5962fbf3672bb6ff922954d8fd469eb8e33
Jesse Brand <jessebrand@gmail.com>
2025-08-11 16:41:04 +0200

    Updated after 16/7/25 and 30/7/25.

--- src/persistency.js
+++ src/persistency.js
@@ -4,27 +4,27 @@ import {LitElement, html, css} from 'lit';
 
 function fetchAllCharacters () {
   return [
-    { name: 'Castle-8', xp: 45, dreamXp: 1},
-    { name: 'Sam', xp: 40, dreamXp: 4 },
+    { name: 'Castle-8', xp: 46, dreamXp: 1},
+    { name: 'Sam', xp: 41, dreamXp: 5 },
     { name: 'Horatio ', xp: 41, dreamXp: 1 },
     { name: 'Lynel', xp: 39 },
     { name: 'Bill', xp: 37, dreamXp: 6 },
-    { name: 'Enphine', xp: 37, dreamXp: 8 },
+    { name: 'Enphine', xp: 38, dreamXp: 9 },
     { name: 'Paxton', xp: 36 },
-    { name: 'Sir Dixon', xp: 36, dreamXp: 8 },
-    { name: 'Ru', xp: 34, dreamXp: 5 },
+    { name: 'Sir Dixon', xp: 37, dreamXp: 9 },
+    { name: 'Ru', xp: 34, dreamXp: 6 },
     { name: 'Alaric', xp: 31 },
     { name: 'Nuk', xp: 27 },
     { name: 'Aboouck', xp: 26 },
     { name: 'Exilian', xp: 26 },
     { name: 'Layden', xp: 23 },
-    { name: 'Bavon', xp: 22 },
-    { name: 'Drathuul', xp: 22 },
-    { name: 'Freya', xp: 22 },
-    { name: 'Girard', xp: 22 },
-    { name: 'John', xp: 22 },
-    { name: 'Navani', xp: 22 },
-    { name: 'Sir Dozak the Magnificient', xp: 0, dreamXp: 5 }
+    { name: 'Bavon', xp: 23 },
+    { name: 'Drathuul', xp: 23 },
+    { name: 'Freya', xp: 23 },
+    { name: 'Girard', xp: 23 },
+    { name: 'John', xp: 23 },
+    { name: 'Navani', xp: 23 },
+    { name: 'Sir Dozak the Magnificient', xp: 0, dreamXp: 6 }
   ]
 }
 
